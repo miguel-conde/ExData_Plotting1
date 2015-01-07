c5c83a5cb07a18a2e3b0c72f2e3d5b04b9e408d8
miguel-conde <miguelco2000@gmail.com>
2015-01-07 17:16:04 +0100

    Terminado plot4

--- plot4.R
+++ plot4.R
@@ -20,30 +20,55 @@ powerConData2Days<-rbind(powerConData2Days, powerConData[as.Date(powerConData$Da
 
 rm(powerConData)
 
-
 times<-powerConData2Days$Time
 dates<-as.Date(powerConData2Days$Date,"%d/%m/%Y")
 dates_times <- paste(dates, times)
 x<-strptime(dates_times, "%Y-%m-%d %H:%M:%S")
 
 plot.new()
+par(mfrow = c(2,2))
+
 with(powerConData2Days, {
+    # Same as plot2 in (1,1)
+    plot(x,as.numeric(as.vector(Global_active_power)),type="l",
+         xlab="",
+         ylab="Global Active Power",
+         cex.lab=0.7,
+         cex.axis=0.7)
+
+    # Voltage plot in (1,2)
+    plot(x,as.numeric(as.vector(Voltage)),type="l",
+         xlab="",
+         ylab="Voltage",
+         cex.lab=0.7,
+         cex.axis=0.7)
+
+    # Same as plot3 in (2,1)
     plot(x,as.numeric(as.vector(Sub_metering_1)),type="l",
          xlab="",
          ylab="Energy sub metering",
-         col="azure4")
+         col="azure4",
+         cex.lab=0.7,
+         cex.axis=0.7)
     lines(x,as.numeric(as.vector(Sub_metering_2)),
           type="l",
           col="red")
     lines(x,as.numeric(as.vector(Sub_metering_3)),
           type="l", 
           col="blue")
+    legend("topright", lty = 1, 
+           col = c("azure4", "red", "blue"), 
+           legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+           cex=0.7)
+
+    # Global_reactive_power in (2,2)
+    plot(x,as.numeric(as.vector(Global_reactive_power)),type="l",
+         xlab="",
+         ylab="Global_reactive_power",
+         cex.lab=0.7,
+         cex.axis=0.7)
 })
 
-legend("topright",  lty=1,
-       col = c("azure4", "red", "blue"), 
-       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-
-dev.copy(png, file = "plot3.png") ## Copy my plot to a PNG file
+dev.copy(png, file = "plot4.png") ## Copy my plot to a PNG file
 dev.off()
 
